ef6a2f82a80ceb4c7b4647cda0e9e3407cf0920e
Shathik131 <sathiksathik126@gmail.com>
2023-11-15 04:24:36 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const API_URL = "http://www.omdbapi.com?i=tt3896198&apikey=250834c2";
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-
+ 
   const searchMovies = async (title) => {
     await axios
       .get(`${API_URL}&s=${title}`)
